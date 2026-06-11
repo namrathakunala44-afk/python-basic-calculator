4aea81dbe4ffe105802877314e798c9ac8e16a92
Kunala Namratha <namrathakunala44@gmail.com>
2026-06-11 20:00:36 +0530

    Add files via upload

---
+++ arithmetic_calc.py
@@ -0,0 +1,36 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-bX
+def mult(a,b):
+    return a*b
+def div(a,b):
+    if(b!=0):
+        return a/b
+    else:
+        return "ERROR division by zero"
+def rem(a,b):
+    return a%b
+while True:
+    print("Enter x,y values(enter numbers only):")
+    x=int(input())
+    y=int(input())
+    print("select an operation(+,-,*,/):")
+    choice=input()
+    if choice=="+":
+        print(f"The addition is{add(x,y)}")
+    elif choice=="-":
+        print(f"The differnce is{sub(x,y)}")
+    elif choice=="*":
+        print(f"The product is{mult(x,y)}")
+    elif choice=="/":
+        print(f"The quotient is{div(x,y)}") 
+    elif choice=="%":
+        print(f"The remainder is{rem(x,y)}")
+    else:
+        print("INVALID CHOICE! PLEASE ENTER A VALID CHOICE")  
+    print("Do you want to perform another calcualtoion?")
+    again=input("Type 'yes' to continue ,or any other key to exit").lower()
+    if again!="yes":
+        print("Goodbye! Thanks for using the calculator.")
+        break
